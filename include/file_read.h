e47e50869a451a079176c1ea932b0488caad7eb7
clemdp <clement.dupuy1997@outlook.fr>
2025-01-09 22:36:43 +0100

    fix: make file read function independant from the caller

--- include/file_read.h
+++ include/file_read.h
@@ -3,7 +3,5 @@
 #include <stdbool.h>
 #include <string.h>
 
-#include "list.h"
-
-int read_file(char* filename, list_t **list_one, list_t **list_two);
+int read_file(char *filename, void (*my_fun)(char *line));
 int get_number_from_string(char *string);
